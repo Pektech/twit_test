d9b9138c5761bb4e91d4883cd6451e1cdc5a0707
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-08-14 00:29:39 -0400

    added views and ip decorator

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
 from app import db
 from .models import User, Tweets
+from flask import request, flash
+from functools import wraps
 import csv
 import datetime
 
@@ -28,3 +30,17 @@ def csv_tweets(csv_file):
             tweet.twt_hash = row['twt_hash']
             db.session.add(tweet)
             db.session.commit()
+
+
+def ip_logging(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        x_real_ip = request.environ.get('HTTP_X_Real_IP')
+        remote_ip = x_real_ip or request.remote_addr
+        timestamp = '{:%Y-%m-%d-%H:%M:%S}'.format(datetime.datetime.now())
+        message = "{0}: Access requested to {1} from IP address (Real IP: {2} " \
+                  "| Remote IP: {3})".format(
+            timestamp, "Users", x_real_ip, remote_ip)
+        flash(message)
+        return func(*args, **kwargs)
+    return decorated_function
